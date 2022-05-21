46e43b9770909d2d37d0943a4ec5564b026f6d09
parnexcodes <parnexcodes@gmail.com>
2022-05-22 00:23:23 +0530

    change gradient colors

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,7 +11,7 @@
 
 <div class="bg-neutral-900 h-screen">
     <div class="flex flex-col justify-center items-center h-screen">
-        <div class="font-exo tracking-wider text-8xl bg-gradient-to-r from-teal-500 via-cyan-600 to-cyan-600 text-transparent bg-clip-text">
+        <div class="font-exo tracking-wider text-8xl bg-gradient-to-r from-[#007CF0] to-[#00DFD8] text-transparent bg-clip-text">
             parnex
         </div>
         <div class="text-md pt-4 text-gray-200">
